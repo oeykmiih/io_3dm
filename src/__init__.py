a6393062ce6b20cf8e305bf43532af107d316709
oeykmiih <mail@kyeo.xyz>
2023-10-21 03:02:07 +0200

    refactor(rhino3dm): support windows/macos plugin in only one packaged plugin

--- src/__init__.py
+++ src/__init__.py
@@ -21,7 +21,7 @@ import sys
 
 
 LIBRARIES = [
-    "rhino3dm",
+    "rhino",
 ]
 
 if LIBRARIES:
---
+++ src/libs/rhino/__init__.py
@@ -0,0 +1,26 @@
+import os
+import platform
+import sys
+
+match platform.system():
+    case 'Windows':
+        DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "win64"))
+    case 'Darwin':
+        match platform.processor():
+            case 'arm':
+                DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "mac_arm"))
+            case 'i386':
+                DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "mac_intel"))
+            case _:
+                print("DEBUG :: Platform not supported by rhino3dm.")
+    case _:
+        print("DEBUG :: Platform not supported by rhino3dm.")
+
+try:
+    if os.path.isdir(DIR) and DIR not in sys.path:
+        sys.path.insert(0, DIR)
+
+    import rhino3dm
+finally:
+    if DIR in sys.path:
+        sys.path.remove(DIR)
