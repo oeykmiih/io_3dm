2c26eebb69367f60e3c2ddb44f94ffb1eb62f99f
oeykmiih <mail@kyeo.xyz>
2023-11-22 10:58:38 +0100

    chore: update rhino3dm to 8.0.1

--- io_3dm/libs/rhino/__init__.py
+++ io_3dm/libs/rhino/__init__.py
@@ -6,13 +6,14 @@ match platform.system():
     case 'Windows':
         DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "win64"))
     case 'Darwin':
-        match platform.processor():
-            case 'arm':
-                DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "mac_arm"))
-            case 'i386':
-                DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "mac_intel"))
-            case _:
-                print("DEBUG :: Platform not supported by rhino3dm.")
+        DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "macos"))
+#        match platform.processor():
+#            case 'arm':
+#                DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "mac_arm"))
+#            case 'i386':
+#                DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "mac_intel"))
+#            case _:
+#                print("DEBUG :: Platform not supported by rhino3dm.")
     case _:
         print("DEBUG :: Platform not supported by rhino3dm.")
 
